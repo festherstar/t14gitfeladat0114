291b111a0801b99f0df9df784b8debead75286fc
festherstar <f.esther94@gmail.com>
2024-01-14 12:14:01 +0100

    Frissítés

--- megoldas.js
+++ megoldas.js
@@ -28,7 +28,7 @@ document.write(`A javascript megértése 100-as skálát tekintve: ${megertes3}`
 
 document.write("<hr>");
 
-//3. feladat
+//3. feladat <-Rosszul értelmeztem a feladatot.
 
 let kezdo=Number (prompt("Adja meg a kezdő értéket:"));
 let vegertek=Number(prompt("Adja meg a végértéket:"));
@@ -37,6 +37,23 @@ for (let i=kezdo; i<=vegertek; i++){
     if (i % 2 === 0){
         document.write(i+",");
     }
+}
+
+document.write("<hr>");
+
+let szamkezdo=Number (prompt("Adja meg a kezdő értéket:"));
+let szamvegertek=Number(prompt("Adja meg a végértéket:"));
+
+let generaltszam=Math.round(Math.random()*(szamvegertek-szamkezdo))+szamkezdo;
+
+if(generaltszam%2==0){
+	document.write(`A generált páros szám a ${szamkezdo}:${szamvegertek} intervallumban:${generaltszam}`);
+}
+else if(generaltszam!=felso){
+	document.write(`A generált páros szám a ${szamkezdo}:${szamvegertek} intervallumban:${generaltszam+1}`);
+}
+else{
+	document.write(`A generált páros szám a ${szamkezdo}:${szamvegertek} intervallumban:${generaltszam-1}`);
 }
 
 document.write("<hr>");
@@ -62,4 +79,35 @@ for (let i = 1; i <= 10 ;i++){
 
 document.write("<hr>");
 
+// <-Eddig sikerült megoldani. Az első feladatot túlkomplikáltam. Egyet pedig elrontottam.
 
+
+//4. feladat
+let eletkor=Number (prompt("Adja meg az életkorát:"));
+
+if(eletkor>0 && eletkor<=6){
+    document.write(`${eletkor} évesen kisgyermekkorban vagy`);
+}
+else if(eletkor>6 && eletkor<=12){
+	document.write(`${eletkor} évesen gyermekkorban vagy`);
+}
+else if(eletkor>12 && eletkor<=16){
+	document.write(`${eletkor} évesen serdülőkorban vagy`);
+}
+else if(eletkor>16 && eletkor<=20){
+	document.write(`${eletkor} évesen ifjúkorban vagy`);
+}
+else if(eletkor>20 && eletkor<=30){
+	document.write(`${eletkor} évesen fiatal felnőtt korban vagy`);
+}
+else if(eletkor>30 && eletkor<=60){
+	document.write(`${eletkor} évesen felnőtt korban vagy`);
+}
+else if(eletkor>60 && eletkor<=120){
+	document.write(`${eletkor} évesen aggkorban vagy`);
+}
+else{
+	document.write(`${eletkor} NEM létezik az adatbázisban mint lehetőség!`);
+} 
+  
+document.write("<hr>");
